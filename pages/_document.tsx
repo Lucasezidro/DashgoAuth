b41b379b2f1a506dab644fe3de72d09b5f5e8470
Lucas Ezidro <you@example.com>
2021-07-23 10:28:31 -0300

    auth

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+
+export default class MyDocument extends Document {
+    render(){
+        return(
+            <Html>
+                <Head>
+                    <title>To-do</title>
+
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Otomanopee+One&display=swap" rel="stylesheet"/>
+
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Otomanopee+One&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"></link>
+                </Head>
+
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
